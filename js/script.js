7776ad885253274277b5ff78bcb2106bad619d1b
rakeshprakash91 <rakeshprakash91@outlook.com>
2016-06-06 15:16:56 +0530

    removed debugger

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 	$window = $(window);
 	$('section[data-type="background"]').each(function () {
 		var $bgobj = $(this); // assigning the object
-		debugger
 		$window.scroll(function () {
 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
 			var coords = '50% ' + yPos + 'px';
